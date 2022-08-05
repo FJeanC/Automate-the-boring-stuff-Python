c40327adc1816169b77ab2c50b039cb28ae78473
FJeanC <jeanfagiani1@gmail.com>
2022-08-05 10:16:56 -0300

    Chapter 15

---
+++ Chapter15/combinedPdfs.py
@@ -0,0 +1,26 @@
+#! python3
+# combinePdfs.py combines all the pdfs in the current working directory into 
+# a single PDF.
+
+import PyPDF2, os
+
+pdfFiles = []
+for filename in os.listdir('.'):
+    if filename.endswith('.pdf'):
+        pdfFiles.append(filename)
+
+pdfFiles.sort(key=str.lower)
+pdfWriter = PyPDF2.PdfFileWriter()
+
+
+for filename in pdfFiles:
+    pdfFileObj = open(filename, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    for pageNum in range(1, pdfReader.numPages):
+        pageObj = pdfReader.getPage(pageNum)
+        pdfWriter.addPage(pageObj)
+    #pdfFileObj.close()
+
+pdfOutput = open('allminutes.pdf', 'wb')
+pdfWriter.write(pdfOutput)
+pdfOutput.close()
